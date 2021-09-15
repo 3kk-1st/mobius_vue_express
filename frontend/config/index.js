c29e7faaf77c0abf965d813aefc13e2216c66d65
3kk-1st <67462317+3kk-1st@users.noreply.github.com>
2021-09-15 15:28:15 +0900

    2fin

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '/home/ebichu/mobius_vue_express/backend/public/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '/home/ebichu/mobius_vue_express/backend/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
