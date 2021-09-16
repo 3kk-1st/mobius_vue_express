b76f4c38f3e7c9c39d116dc9bcf04e39339ed6d0
3kk-1st <3kk1st@gamil.com>
2021-09-16 15:04:41 +0900

    last

---
+++ frontend/SignUp.vue
@@ -0,0 +1,44 @@
+<div class = "input_row">
+	<label for ="id"> 아이디 </label>
+	<input type = "text" id="id" v-modle = "user.userid">
+</div>
+<div class = "input_row">
+        <label for ="name"> 이름 </label>
+        <input type = "text" id="name" v-modle = "user.name">
+</div>
+<div class = "input_row">
+        <label for ="password">  비밀번호 </label>
+        <input type = "password" id="id" v-modle = "user.password">
+</div>
+<button :click="signUp">가입하기</button>
+<script>
+data : function () {
+return {
+user: {
+userid: '',
+name : '',
+password: ''
+}
+}
+},
+
+methods: {
+        signUp: function (event) {
+                this.$http.post('/api/users/signUp', {
+                        user: this.user
+                })
+                .then((res) => {
+                        if (res.data.success == ture) {
+                                alert(res.data.message);
+                        this.$router.push('/login')
+                        }
+                        if (res.data.success == false)  {
+                                alert(res.data.message);
+                        }
+                })
+                .catch(function (error) {
+                alert('error')
+                })
+        }
+}
+</script>
---
+++ frontend/userList.vue
@@ -0,0 +1,20 @@
+data () {
+  return {
+    users: []
+  }
+},
+created () {
+  this.$http.get('/api/users')
+    .then((response) => {
+      this.users = response.data
+    })
+}
+<div v-for="(user, index) in users" :key="index" class="user-wrap">
+   <h2>No. {{index + 1}}</h2>
+   <dl>
+      <dt>아이디</dt>
+      <dd>{{user.userid}}</dd>
+      <dt>이름</dt>
+      <dd>{{user.name}}</dd>
+   </dl>
+</div>
